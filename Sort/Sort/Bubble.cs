4716fe63f0c856beebc01b0991d5878467141756
spainior <ice_shi@163.com>
2020-08-19 23:20:17 +0800

    Add sort project for sorting algrithm

---
+++ Sort/Sort/Bubble.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sort
+{
+    class Bubble
+    {
+        public Bubble()
+        { }
+
+        //This is C# bubble sorting algrithm.
+        public int[] BubbleSorting(int[] arr)
+        {
+            int temp;
+            for (int i =0; i<arr.Length-1; i++)
+            {
+                for (int j = 0; j<arr.Length - 1 -i; j++)
+                {
+                    if (arr[j]>arr[j+1])
+                    {
+                        temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                        
+                    }
+
+                }
+            }
+
+            return arr;
+        }
+        
+
+    }
+}
---
+++ Sort/Sort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is for sorting algrithim");
+
+            int[] arr = new int[] { 3, 2, 5, 6, 7, 8, };
+            Console.WriteLine($"The sample array is {arr}");
+            print(arr);
+
+            Console.WriteLine("The first one is Bubble Sorting");
+
+            Bubble bubble = new Bubble();
+
+            print(bubble.BubbleSorting(arr));
+            Console.ReadLine();
+
+        }
+
+        //abstract print element in the array.
+        static void print(int[] arr)
+        {
+            foreach (int n in arr)
+            {
+                Console.Write(n);
+                Console.Write(" ");
+            }
+        }
+    }
+}
